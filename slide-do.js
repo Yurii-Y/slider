08d15d62fc021cb9d1bbed8ff85773f9e3e051ad
Yurii Yanushevskyi <yanushevskyiyurii@gmail.com>
2023-01-09 23:03:29 +0200

    commit all files slider

---
+++ slide-do.js
@@ -0,0 +1,45 @@
+const body = document.body;
+const slides = document.querySelectorAll(".slide");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
+
+let activeSlide = 0;
+
+//events listener
+rightBtn.addEventListener('click', rightSlide);
+leftBtn.addEventListener('click', leftSlide);
+
+//function
+
+function rightSlide() {
+  activeSlide++;
+
+  if(activeSlide > slides.length - 1) {
+    activeSlide = 0
+  }
+
+  setBgToBody();
+  setActiveSlides()
+};
+
+function leftSlide() {
+  activeSlide--;
+
+  if(activeSlide < 0) {
+    activeSlide = slides.length - 1;
+  }
+
+  setBgToBody();
+  setActiveSlides()
+};
+
+setBgToBody();
+
+function setBgToBody() {
+  body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+
+function setActiveSlides() {
+  slides.forEach((slide) => slide.classList.remove("active"));
+  slides[activeSlide].classList.add("active");
+}
